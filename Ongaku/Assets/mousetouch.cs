8de95b9554c7d96a2edff4153ccfb7608102d593
check1996 <check280045830@gmail.com>
2018-01-27 01:41:01 -0600

    chang-1

---
+++ Ongaku/Assets/mousetouch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mousetouch: MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                //Select Stage
+                if (hit.transform.parent.name == "All_Planets")
+                {
+                    Debug.Log("yeah yeah yeah");
+                }
+            }
+        }
+
+    }
+}
