382b4f369ca58f977c188decddef4855f750916b
llemke216 <35860238+llemke216@users.noreply.github.com>
2018-01-27 11:53:30 -0600

    add basic UI

---
+++ Ongaku/Assets/Scripts/ModalPanel.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class ModalPanel : MonoBehaviour {
+    public Text question; //Will need to change this
+    public Image iconImage;
+    public Button yesButton;
+    public Button noButton;
+    public Button cancelButton;
+    public Button lastButton;
+    public GameObject modalPanelObject;
+
+    private static ModalPanel modalPanel;
+
+    public static ModalPanel Instance()
+    {
+        if (!modalPanel)
+        {
+            modalPanel = FindObjectOfType(typeof(ModalPanel)) as ModalPanel;
+            if (!modalPanel)
+            {
+                Debug.LogError("There needs to be one active ModalPanel script on a GameObject in your scene.");
+            }
+        }
+        return modalPanel;
+    }
+
+    //Yes/No/Cancel: a string, a yes,. a no, and a cancel event
+    public void Choice(string question, UnityAction yesEvent, UnityAction noEvent, UnityAction cancelEvent, UnityAction lastEvent)
+    {
+        modalPanelObject.SetActive(true);
+
+        yesButton.onClick.RemoveAllListeners();
+        yesButton.onClick.AddListener(yesEvent);
+        yesButton.onClick.AddListener(ClosePanel);
+
+        noButton.onClick.RemoveAllListeners();
+        noButton.onClick.AddListener(noEvent);
+        noButton.onClick.AddListener(ClosePanel);
+
+        cancelButton.onClick.RemoveAllListeners();
+        cancelButton.onClick.AddListener(cancelEvent);
+        cancelButton.onClick.AddListener(ClosePanel);
+
+        lastButton.onClick.RemoveAllListeners();
+        lastButton.onClick.AddListener(lastEvent);
+        lastButton.onClick.AddListener(ClosePanel);
+
+        this.question.text = question;
+
+        this.iconImage.gameObject.SetActive(false);
+        yesButton.gameObject.SetActive(true);
+        noButton.gameObject.SetActive(true);
+        cancelButton.gameObject.SetActive(true);
+        lastButton.gameObject.SetActive(true);
+    }
+
+    void ClosePanel()
+    {
+        modalPanelObject.SetActive(false);
+    }
+}
